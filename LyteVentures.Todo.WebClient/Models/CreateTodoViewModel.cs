4a6564638d904bb27923491cd4560c591f6e0269
Mirza Ghulam Rasyid <mirza.rasyid@kobkiat-it.com>
2022-01-24 19:12:11 +0700

    Adjusted display attribute

--- LyteVentures.Todo.WebClient/Models/CreateTodoViewModel.cs
+++ LyteVentures.Todo.WebClient/Models/CreateTodoViewModel.cs
@@ -9,8 +9,10 @@ namespace LyteVentures.Todo.WebClient.Models
         public string Title { get; set; }
         public string Description { get; set; }
         [Required]
+        [Display(Name = "Start Schedule")]
         public DateTime StartSchedule { get; set; }
         [Required]
+        [Display(Name = "End Schedule")]
         public DateTime EndSchedule { get; set; }
     }
 }
